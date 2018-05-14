6885f3801f73159d08790ecb5b52df98357a4a8f
liyl <liyl@fengpb>
2018-05-15 07:31:21 +0800

    9

--- sixcore/src/main/java/com/sixframe/core/Swagger.java
+++ sixcore/src/main/java/com/sixframe/core/Swagger.java
@@ -12,7 +12,9 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Parameter;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -35,6 +37,11 @@ public class Swagger {
         return new ApiInfoBuilder()
                 .version("1.0")
                 .build();
+    }
+    
+    @Bean
+    public SecurityScheme apiKey() {
+    	return new ApiKey("access_token", "accessToken", "header");
     }
     
     private List<Parameter> setHeaderToken() {
